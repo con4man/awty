9be68b9b62aa1d03a3885c0a8e7f8956840ff785
connor stephen miles fitzgerald hawthorne <connorha@uw.edu>
2018-05-14 15:42:04 -0700

    Spams using SMS messages instead of toasts now
    Permissions need to be granted in order for SMS messages to be spammed

--- app/src/main/java/textspammer/connorha/washington/edu/textspammer/AlarmReceiver.kt
+++ app/src/main/java/textspammer/connorha/washington/edu/textspammer/AlarmReceiver.kt
@@ -3,13 +3,34 @@ package textspammer.connorha.washington.edu.textspammer
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.telephony.SmsManager
 import android.widget.Toast
+import android.app.PendingIntent
 
 class AlarmReceiver : BroadcastReceiver() {
+    private val maxSmsLength = 160
+    private val smsSent = "SMS_SENT"
+    private val smsDelivered = "SMS_DELIVERED"
     override fun onReceive(context: Context, intent: Intent) {
         val numberToText = intent.getStringExtra("numberToText")
         val messageString = intent.getStringExtra("messageString")
-        val formattedPhoneNumber = "(${numberToText.substring(0, 3)}) ${numberToText.substring(3, 6)}-${numberToText.substring(6)}"
-        Toast.makeText(context, "$formattedPhoneNumber: $messageString", Toast.LENGTH_SHORT).show()
+        val smsManager = SmsManager.getDefault()
+        val piSent = PendingIntent.getBroadcast(context, 0, Intent(smsSent), 0)
+        val piDelivered = PendingIntent.getBroadcast(context, 0, Intent(smsDelivered), 0)
+
+        try {
+            if (messageString.length < maxSmsLength) {
+                smsManager.sendTextMessage(numberToText, null, messageString, piSent, piDelivered)
+                Toast.makeText(context, "SMS sent.",
+                        Toast.LENGTH_LONG).show()
+            } else {
+                val messagePartsList = smsManager.divideMessage(messageString)
+                smsManager.sendMultipartTextMessage(numberToText, null, messagePartsList, null, null)
+                Toast.makeText(context, "SMS sent.",
+                        Toast.LENGTH_LONG).show()
+            }
+        } catch (e: Exception) {
+            Toast.makeText(context, "Something went wrong when sending SMS", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/textspammer/connorha/washington/edu/textspammer/MainActivity.kt
+++ app/src/main/java/textspammer/connorha/washington/edu/textspammer/MainActivity.kt
@@ -4,8 +4,10 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.text.Editable
 import android.text.TextWatcher
 import kotlinx.android.synthetic.main.activity_main.*
@@ -72,21 +74,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun setSwitchButtonOnClickListener() {
         buttonSwitch.setOnClickListener{
-            val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-            val intent = Intent(this, AlarmReceiver::class.java)
-            if (!appIsRunning) {
-                intent.putExtra("numberToText", numberToText)
-                intent.putExtra("messageString", messageString)
-                val alarmFrequency = spammerFrequency.toString().toLong() * 1000 * 60
-                val alarmIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-                alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), alarmFrequency, alarmIntent)
-                appIsRunning = true
-            } else {
-                val alarmIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
-                alarmManager.cancel(alarmIntent)
-                appIsRunning = false
+            if (smsPermissionIsGranted()) {
+                val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+                val intent = Intent(this, AlarmReceiver::class.java)
+                if (!appIsRunning) {
+                    intent.putExtra("numberToText", numberToText)
+                    intent.putExtra("messageString", messageString)
+                    //converts frequency from minutes to milliseconds
+                    val alarmFrequency = spammerFrequency.toString().toLong() * 1000 * 60
+                    val alarmIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+                    alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), alarmFrequency, alarmIntent)
+                    appIsRunning = true
+                } else {
+                    val alarmIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+                    alarmManager.cancel(alarmIntent)
+                    appIsRunning = false
+                }
+                changeSwitchButtonText()
             }
-            changeSwitchButtonText()
         }
     }
 
@@ -119,6 +124,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun spammerFrequencyIsValid() :  Boolean {
         return spammerFrequency in 1..100
+    }
+
+    private fun smsPermissionIsGranted () : Boolean {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            //requests permission to send sms
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.SEND_SMS), 1)
+            return false
+        } else {
+            return true
+        }
     }
 
 }
